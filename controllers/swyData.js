bf9f374798847e611b5f5867c33e221e9750c253
lyt439172 <lyt439172@qq.com>
2021-08-19 14:01:04 +0800

    小调整

--- controllers/swyData.js
+++ controllers/swyData.js
@@ -32,18 +32,17 @@ module.exports = {
             reason: ctx.request.body.reason,
             position: ctx.request.body.position,
             status: ctx.request.body.status,
-            solution: ctx.request.body.solution,
-            // cover: ctx.request.body.cover,
+            solution: ctx.request.body.solution
         })
         ctx.body = new RetJson(0, 'success', swyData)
     },
 
-    async  index(ctx){
+    async index(ctx){
         const swyData = await SwyData.getSwyData(ctx.query.pg)
         ctx.body = new RetJson(0, 'success', swyData)
     },
 
-    async  show(ctx){
+    async show(ctx){
         const swyData = await SwyData.getSwyDataById(ctx.params.id)
         ctx.body = new RetJson(0, 'success', swyData)
     },
